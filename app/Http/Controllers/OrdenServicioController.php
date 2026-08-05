b074eb8e36c240a5090b31bddd9d55025a7a05a7
Guillermo Mosquera <gjmosquerac@gmail.com>
2026-08-04 20:25:22 -0400

    Actualizar controlador con enlace web para el sistema

--- app/Http/Controllers/OrdenServicioController.php
+++ app/Http/Controllers/OrdenServicioController.php
@@ -10,6 +10,8 @@ class OrdenServicioController extends Controller
     {
         $telefonoLimpio = "584245652208";
 
+        $urlSistema = url('/ordenes');
+
         $mensaje = "*LEOTEC REFRIGERACIÓN - NUEVA SOLICITUD*\n\n" .
                    "*Cliente:* {$request->nombre}\n" .
                    "*Teléfono:* {$request->telefono}\n" .
@@ -17,7 +19,7 @@ class OrdenServicioController extends Controller
                    "*Tipo de Equipo:* {$request->tipo_equipo}\n" .
                    "*Marca:* " . ($request->marca ?? 'No especificada') . "\n" .
                    "*Falla Reportada:* {$request->falla}\n\n" .
-                   "_Atiende la orden desde el sistema web._";
+                   "Atiende la orden desde el sistema web:\n" . $urlSistema;
 
         $urlWhatsApp = "https://wa.me/" . $telefonoLimpio . "?text=" . rawurlencode($mensaje);
 
