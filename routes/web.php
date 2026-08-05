ab5299328862c052dfcd3355566aaac1623574ca
Guillermo Mosquera <gjmosquerac@gmail.com>
2026-08-04 20:37:21 -0400

    Definir ruta login y limpiar archivo web.php

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ use App\Http\Controllers\OrdenServicioController;
 Route::get('/', [OrdenServicioController::class, 'formCliente'])->name('cliente.solicitud');
 Route::post('/solicitud/guardar', [OrdenServicioController::class, 'guardarSolicitud'])->name('solicitud.guardar');
 
+Route::get('/login', function () {
+    return redirect('/');
+})->name('login');
+
 Route::middleware(['auth'])->group(function () {
     Route::get('/ordenes/crear', [OrdenServicioController::class, 'create'])->name('ordenes.create');
     Route::post('/ordenes/guardar', [OrdenServicioController::class, 'store'])->name('ordenes.store');
